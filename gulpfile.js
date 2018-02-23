a4df6d0fbf5a0524743fd0d8c68ba69cfa63030f
JesseWeb <w18674555453@gmail.com>
2018-02-23 10:19:24 +0800

    demo commit

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+let gulp = require('gulp')
+let babel = require("gulp-babel")
+let pump = require('pump')
+let uglify = require('gulp-uglify')
+let sourcemaps = require('gulp-sourcemaps');
+let plumber = require("gulp-plumber")
+let browserSync = require('browser-sync')
+let reload = browserSync.reload;
+let cleanCSS = require('gulp-clean-css')
+let gulpSequence = require('gulp-sequence')
+const path = require('path')
+
+gulp.task('jsUglify', function () {
+    pump([
+        gulp.src('./js/main.js'),
+        plumber(),
+        sourcemaps.init(),
+        babel({
+            presets: ['env']
+        }),
+        uglify(),
+        sourcemaps.write('./maps'),
+        gulp.dest('./static'),
+        reload({
+            stream: true
+        }),
+    ])
+})
+gulp.task('cssMinify', function () {
+    pump([
+        gulp.src('./style/main.css'),
+        plumber(),
+        sourcemaps.init(),
+        cleanCSS({
+            compatibility: 'ie8'
+        }),
+        sourcemaps.write('./maps'),
+        gulp.dest('./static'),
+        reload({
+            stream: true
+        })
+    ])
+
+});
+
+function jsUglify(src) {
+    let dist = path.relative(__dirname,'./static')
+    pump([
+        gulp.src(src),
+        plumber({
+            errorHandler(err, callback) {
+                console.log(err)
+            }
+        }),
+        sourcemaps.init(),
+        babel({
+            presets: ["env"],
+            plugins: ["syntax-async-functions"]
+        }),
+        uglify(),
+        sourcemaps.write('maps'),
+        gulp.dest(dist),
+        reload({
+            stream: true
+        }),
+    ])
+}
+
+function cssMinify(src) {
+    let dist = path.relative(__dirname,'./static')
+    pump([
+        gulp.src(src),
+        plumber({
+            errorHandler(err, callback) {
+                console.log(err)
+            }
+        }),
+        sourcemaps.init(),
+        cleanCSS({
+            compatibility: 'ie8'
+        }),
+        sourcemaps.write('maps'),
+        gulp.dest(dist),
+        reload({
+            stream: true
+        }),
+    ])
+}
+gulp.task('default', function (cb) {
+    gulpSequence('jsUglify', 'cssMinify', () => {
+        browserSync.init({
+            port: 80,
+            server: {
+                baseDir: "./",
+            },
+        });
+        gulp.watch('./js/main.js').on('change', function (e) {
+            jsUglify(e.path)
+        })
+        gulp.watch(['./js/main.js','./index.html']).on('change', reload);
+        gulp.watch('./style/main.css').on('change', function (e) {
+            cssMinify(e.path)
+        })
+    })
+})
---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function () {
+    let Animation = function () {
+        $('.step1 > div').addClass('fadeIn')
+        $('.step').each((index, el) => {
+            $(el).css({ "z-index": $('.step').length - index })
+        })
+
+
+    }
+    Animation.prototype = {
+        step: 1,
+        animating: false,
+        next: function () {
+            if (this.step >= $('.step').length) {
+                return
+            }
+            if (!this.animating) {
+                if (this.step >= $('.step').length - 1) {
+                    $('.next').hide()
+                }
+                $('.step' + this.step).removeClass('getBackHere').addClass('fallsDown')
+                this.animating = true
+                this.step++
+                setTimeout(() => {
+                    this.animating = false
+                    $('.prev').show()
+                }, 1000)
+            }
+        },
+        prev: function () {
+            if (this.step <= 1) {
+                return
+            }
+            if (!this.animating) {
+                if (this.step <= 2) {
+                    $('.prev').hide()
+                }
+                this.step--
+                $('.step' + this.step).removeClass('fallsDown').addClass('getBackHere')
+                this.animating = true
+                setTimeout(() => {
+                    this.animating = false
+                    $('.next').show()
+                }, 400)
+            }
+        },
+    }
+    let animation = new Animation()
+    $('.next').click((e) => {
+        animation.next()
+    })
+    $('.prev').click((e) => {
+        animation.prev()
+    })
+
+
+})
